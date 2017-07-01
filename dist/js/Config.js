455c8818b0044450091e1809c1b50978c10ed2d7
Paul Nechifor <paul@nechifor.net>
2017-07-01 22:30:31 +0100

    Add dist.

---
+++ dist/js/Config.js
@@ -0,0 +1,17 @@
+Config = {
+    "MOVE_SPEED": 15,
+    "CELL_SIZE": 0.1,
+    "Computers": {
+        "Green": {
+            "cells": 1280,
+            "circuitTexture": "textures/circuit.png",
+            "gridTexture": "textures/grid.png",
+            "lightCycleTextures": [
+                "textures/light_cycle_01.png",
+                "textures/light_cycle_02.png",
+                "textures/light_cycle_03.png",
+                "textures/light_cycle_04.png"
+            ]
+        }
+    }
+};
