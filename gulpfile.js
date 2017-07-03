924bef8c04b57714f1b85aff6943b00288288c87
Paul Nechifor <paul@nechifor.net>
2017-07-03 22:51:38 +0100

    Add the new dist.

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,6 @@ const webpack = require('webpack-stream');
 const webpackConfig = require('./webpack.config.js');
 const webserver = require('gulp-webserver');
 
-const dist = 'dist2';
-
 gulp.task('default', ['html', 'webserver', 'watch']);
 
 gulp.task('build', ['js', 'html', 'lib', 'textures']);
@@ -16,28 +14,28 @@ gulp.task('build', ['js', 'html', 'lib', 'textures']);
 gulp.task('js', () => {
   return gulp.src('src/index.js')
     .pipe(webpack(webpackConfig))
-    .pipe(gulp.dest(dist));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('html', () => {
   return gulp.src('index.pug')
     .pipe(pug({}))
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest(dist));
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('lib', () => {
-  return gulp.src('lib/**.js').pipe(gulp.dest(dist))
+  return gulp.src('lib/**.js').pipe(gulp.dest('dist'))
 });
 
 gulp.task('textures', () => {
   return gulp.src('src/textures/**/*')
-  .pipe(gulp.dest(path.join(dist, 'textures')));
+  .pipe(gulp.dest('dist/textures'));
 });
 
 gulp.task('webserver', () => {
   const port = parseInt(process.env.port || '8080', 10);
-  return gulp.src(dist)
+  return gulp.src('dist')
     .pipe(webserver({ livereload: true, open: true, port, host: '0.0.0.0' }));
 });
 
